c50af3d982afc7ad5ac1b1f2746253337561e527
preludelife <preludelife@f23193c2-f3dc-b4fb-65cf-9d36aa571921>
2011-01-19 08:02:24 +0000

    ibatis, mybatis로 dao 단건 조회

---
+++ trunk/developerhaus.repository/src/main/java/developerhaus/repository/ibatis/dao/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package developerhaus.repository.ibatis.dao;
+
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import developerhaus.domain.User;
+
+/**
+ * USER 정보의 등록/수정/삭제/조회 처리를 위한 DAO implementation 이다.
+ * ibatis의 SqlMapClientDaoSupport를 상속함
+ * 
+ * @author Juyeong, Seo
+ */
+public class UserDaoImpl extends SqlMapClientDaoSupport implements UserDao {
+
+	/**
+	 * User 정보를 조회한다.
+	 * 
+	 * @param id
+	 * @return User
+	 */
+	@Override
+	public User getUser(String id) {
+		return (User) getSqlMapClientTemplate().queryForObject("findMemberById", id);
+	}
+
+}
