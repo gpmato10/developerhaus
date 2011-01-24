15b6d05db58a6f54fa9918d88e30389eed5460e4
preludelife <preludelife@f23193c2-f3dc-b4fb-65cf-9d36aa571921>
2011-01-24 11:08:11 +0000

    테스트중입니다.

---
+++ developerhaus.repository/src/main/java/developerhaus/repository/mybatis/UserRepository.java
@@ -0,0 +1,8 @@
+package developerhaus.repository.mybatis;
+
+import developerhaus.domain.User;
+import developerhaus.repository.api.GenericRepository;
+
+public interface UserRepository extends GenericRepository<User, Integer> {
+
+}
