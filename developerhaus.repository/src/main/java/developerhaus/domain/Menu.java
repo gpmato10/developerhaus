d3e5747b3d73bc1c3181ec40d7a2501abba12110
want813 <want813@gmail.com>
2011-02-12 12:05:10 +0000

    도메인 패키지로 이동...

---
+++ developerhaus.repository/src/main/java/developerhaus/domain/Menu.java
@@ -0,0 +1,20 @@
+/**========================================================
+*파일명         : Menu.java
+*파일용도       : 
+*
+*마지막변경일자 : 2011. 2. 12.
+*마지막변경자   : want
+=========================================================*/
+package developerhaus.domain;
+
+/**
+ * @author want
+ *
+ */
+public class Menu {
+
+	private int menuId;
+	private String menuName;
+	private int upperMenuId;
+	
+}
